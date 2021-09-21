4748668c3cd7e3e4508a8a85c578afe46a4d47f2
mamat5592 <75572925+mamat5592@users.noreply.github.com>
2021-09-21 20:35:17 +0430

    create 3dmodel's model/migration

---
+++ database/migrations/2021_09_21_152637_create_three_d_models_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThreeDModelsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('three_d_models', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->text('specification');
+            $table->integer('view');
+            $table->integer('download');
+            $table->decimal('price');
+            $table->integer('like');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('three_d_models');
+    }
+}
