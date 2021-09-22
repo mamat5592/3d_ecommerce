252d35e3e5bd66aae76502c2ba2b5d5db2e92e7d
mamat5592 <75572925+mamat5592@users.noreply.github.com>
2021-09-22 11:34:54 +0330

    create category - 3d model pivot table

---
+++ database/migrations/2021_09_22_075854_create_category_three_d_model_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryThreeDModelTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('category_three_d_model', function (Blueprint $table) {
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('three_d_model_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('category_three_d_model');
+    }
+}
