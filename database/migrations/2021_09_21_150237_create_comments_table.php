fd70a0ae35d063e3b7f10bcabda43f791905ee14
mamat5592 <75572925+mamat5592@users.noreply.github.com>
2021-09-22 12:12:56 +0330

    create relation between comment and user

--- database/migrations/2021_09_21_150237_create_comments_table.php
+++ database/migrations/2021_09_21_150237_create_comments_table.php
@@ -10,6 +10,7 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->text('text');
             $table->unsignedBigInteger('reply')->nullable();
             $table->timestamps();
