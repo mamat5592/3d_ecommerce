5385ae4eaee138b4f603092ba6d78220caca534c
mamat5592 <75572925+mamat5592@users.noreply.github.com>
2021-09-22 13:29:47 +0330

    create tag - 3d model pivot table

---
+++ database/migrations/2021_09_22_095839_create_tag_three_d_model_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagThreeDModelTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('tag_three_d_model', function (Blueprint $table) {
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
+            $table->foreignId('three_d_model_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('tag_three_d_model');
+    }
+}
