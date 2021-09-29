d401e7face5ad10d465f09c18b11b1ba1d5e1fa3
mamat5592 <75572925+mamat5592@users.noreply.github.com>
2021-09-29 23:20:43 +0330

    some change in erd and role migration

--- database/migrations/2021_09_27_014757_create_roles_table.php
+++ database/migrations/2021_09_27_014757_create_roles_table.php
@@ -10,9 +10,8 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->text('description')->nullable();
-            $table->tinyInteger('priority')->unique();
             $table->timestamps();
         });
     }
