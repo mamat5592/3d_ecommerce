9f22a044139c9da24495651ba59cba1cc95219a9
mamat5592 <75572925+mamat5592@users.noreply.github.com>
2021-10-02 16:51:59 +0330

    role controller

---
+++ app/Http/Controllers/Api/V1/RoleController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RoleStoreRequest;
+use App\Http\Requests\RoleUpdateRequest;
+use App\Http\Resources\RoleCollection;
+use App\Http\Resources\RoleResource;
+use App\Models\Role;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    public function index()
+    {
+        $roles = Role::all();
+
+        if(auth()->user()->cannot('viewAny', $roles)){
+            return response(['message' => 'not authorized'], 403);
+        }
+
+        return new RoleCollection($roles);
+    }
+
+    public function store(RoleStoreRequest $request)
+    {
+        if($request->user->cannot('create')){
+            return response(['message' => 'not authorized'], 403);
+        }
+
+        $validated = $request->validated();
+
+        return Role::create($validated);
+    }
+
+    public function show($id)
+    {
+        $role = Role::findOrFail($id);
+
+        if(auth()->user()->cannot('viewAny', $role)){
+            return response(['message' => 'not authorized'], 403);
+        }
+
+        return new RoleResource($role);
+    }
+
+    public function update(RoleUpdateRequest $request, $id)
+    {
+        $role = Role::findOrFail($id);
+
+        if(auth()->user()->cannot('update', $role)){
+            return response(['message' => 'not authorized'], 403);
+        }
+
+        $validated = $request->validated();
+
+        return $role->update($validated);
+    }
+
+    public function destroy($id)
+    {
+        $role = Role::findOrFail($id);
+
+        if(auth()->user()->cannot('delete', $role)){
+            return response(['message' => 'not authorized'], 403);
+        }
+
+        return $role->delete();
+    }
+}
