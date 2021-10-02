f0f396701c1eb61c314fa64962a01a7d672a823c
mamat5592 <75572925+mamat5592@users.noreply.github.com>
2021-10-02 21:22:15 +0330

    add bookmark controller

---
+++ app/Http/Controllers/Api/V1/BookmarkController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\BookmarkCollection;
+use App\Http\Resources\BookmarkResource;
+use App\Models\Bookmark;
+use Illuminate\Http\Request;
+
+class BookmarkController extends Controller
+{
+    public function index()
+    {
+        if (auth()->user()->cannot('viewAny')) {
+            return response(['message' => 'not authorized'], 403);
+        }
+
+        return new BookmarkCollection(Bookmark::all());
+    }
+
+    public function store(Request $request)
+    {
+        if ($request->user()->cannot('create')) {
+            return response(['message' => 'not authorized'], 403);
+        }
+
+        $validated = $request->validated();
+
+        return Bookmark::create($validated);
+    }
+
+    public function show($id)
+    {
+        $bookmark = Bookmark::findOrFail($id);
+
+        if (auth()->user()->cannot('view', $bookmark)) {
+            return response(['message' => 'not authorized'], 403);
+        }
+
+        return new BookmarkResource($bookmark);
+    }
+
+    public function update(Request $request, $id)
+    {
+        if ($request->user()->cannot('update')) {
+            return response(['message' => 'not authorized'], 403);
+        }
+
+        $validated = $request->validated();
+        $bookmark = Bookmark::findOrFail($id);
+
+        return $bookmark->update($validated);
+    }
+
+    public function destroy($id)
+    {
+        if (auth()->user()->cannot('delete')) {
+            return response(['message' => 'not authorized'], 403);
+        }
+        
+        $bookmark = Bookmark::findOrFail($id);
+
+        return $bookmark->delete();
+    }
+}
