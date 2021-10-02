35c7ce3222c05b4345a352a4c866afdceeeec0cd
mamat5592 <75572925+mamat5592@users.noreply.github.com>
2021-10-02 21:36:34 +0330

    add cart requests

---
+++ app/Http/Requests/CartStoreRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CartStoreRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'user_id' => ['required', 'integer', 'exists:users,id'],
+            'three_d_model_id' => ['required', 'integer', 'exists:three_d_models,id']
+        ];
+    }
+
+    public function messages()
+    {
+        return [];
+    }
+}
---
+++ app/Http/Requests/CartUpdateRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CartUpdateRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'user_id' => ['integer', 'exists:users,id'],
+            'three_d_model_id' => ['integer', 'exists:three_d_models,id']
+        ];
+    }
+
+    public function messages()
+    {
+        return [];
+    }
+}
