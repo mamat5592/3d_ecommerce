156dd00c64dd6a12f166c70d82a1e1b96beb98fe
mamat5592 <75572925+mamat5592@users.noreply.github.com>
2021-10-04 17:47:27 +0330

    add role modifire to user

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\UserUpdateRequest;
 use App\Http\Resources\UserCollection;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
@@ -64,5 +65,43 @@ class UserController extends Controller
         }
 
         return $user->delete();
+    }
+
+    public function add_role(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $validated = $request->validate([
+            'role_id' => ['required', 'integer', 'numeric', 'exists:roles,id']
+        ]);
+
+        if ($request->user()->cannot('add_role', $user)) {
+            return response(['message' => 'not authorized'], 403);
+        }
+
+        $user->roles()->attach($validated['role_id']);
+
+        return response(['message' => 'role added to user seccessfully.'], 200);
+    }
+
+    public function remove_role(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $validated = $request->validate([
+            'role_id' => ['required', 'integer', 'numeric', 'exists:roles,id']
+        ]);
+
+        if(!$user->roles->contains($validated['role_id'])){
+            return response(['message' => 'user has not that role'], 403);
+        }
+
+        if ($request->user()->cannot('remove_role', $user)) {
+            return response(['message' => 'not authorized'], 403);
+        }
+
+        $user->roles()->detach($validated['role_id']);
+
+        return response(['message' => 'user role removed seccessfully.'], 200);
     }
 }
