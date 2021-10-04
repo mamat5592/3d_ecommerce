d37f037955ad3682ecf8a05b4cec97635bb6fa76
mamat5592 <75572925+mamat5592@users.noreply.github.com>
2021-10-04 16:41:37 +0330

    add permission modifier to role

--- app/Http/Controllers/Api/V1/RoleController.php
+++ app/Http/Controllers/Api/V1/RoleController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\RoleStoreRequest;
 use App\Http\Requests\RoleUpdateRequest;
 use App\Http\Resources\RoleCollection;
 use App\Http\Resources\RoleResource;
+use App\Models\Permission;
 use App\Models\Role;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,7 @@ class RoleController extends Controller
 {
     public function index()
     {
-        if(auth()->user()->cannot('viewAny', $roles)){
+        if (auth()->user()->cannot('viewAny')) {
             return response(['message' => 'not authorized'], 403);
         }
 
@@ -23,7 +24,7 @@ class RoleController extends Controller
 
     public function store(RoleStoreRequest $request)
     {
-        if($request->user()->cannot('create')){
+        if ($request->user()->cannot('create')) {
             return response(['message' => 'not authorized'], 403);
         }
 
@@ -36,7 +37,7 @@ class RoleController extends Controller
     {
         $role = Role::findOrFail($id);
 
-        if(auth()->user()->cannot('viewAny', $role)){
+        if (auth()->user()->cannot('viewAny', $role)) {
             return response(['message' => 'not authorized'], 403);
         }
 
@@ -47,7 +48,7 @@ class RoleController extends Controller
     {
         $role = Role::findOrFail($id);
 
-        if($request->user()->cannot('update', $role)){
+        if ($request->user()->cannot('update', $role)) {
             return response(['message' => 'not authorized'], 403);
         }
 
@@ -60,10 +61,44 @@ class RoleController extends Controller
     {
         $role = Role::findOrFail($id);
 
-        if(auth()->user()->cannot('delete', $role)){
+        if (auth()->user()->cannot('delete', $role)) {
             return response(['message' => 'not authorized'], 403);
         }
 
         return $role->delete();
+    }
+
+    public function add_permission(Request $request, $id)
+    {
+        $role = Role::findOrFail($id);
+
+        $validated = $request->validate([
+            'permission_id' => ['required', 'integer', 'numeric', 'exists:permissions,id']
+        ]);
+
+        if ($request->user()->cannot('add_permission', $role)) {
+            return response(['message' => 'not authorized'], 403);
+        }
+
+        $role->permissions()->attach($validated['permission_id']);
+
+        return response(['message' => 'permission added to role seccessfully.'], 200);
+    }
+
+    public function remove_permission(Request $request, $id)
+    {
+        $role = Role::findOrFail($id);
+
+        $validated = $request->validate([
+            'permission_id' => ['required', 'integer', 'numeric', 'exists:permissions,id', 'exists:permission_role,permission_id']
+        ]);
+
+        if ($request->user()->cannot('remove_permission', $role)) {
+            return response(['message' => 'not authorized'], 403);
+        }
+
+        $role->permissions()->detach($validated['permission_id']);
+
+        return response(['message' => 'role permission removed seccessfully.'], 200);
     }
 }
